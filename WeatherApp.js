337d989e74e1599a7f84dfed0a7cf72f55374774
Evan Kleiner <gkleinereva@gmail.com>
2017-11-13 14:53:24 -0800

    Added function to get Location data on website load

--- WeatherApp.js
+++ WeatherApp.js
@@ -1,1 +1,14 @@
-//this is javascript
+$(document).ready(function() {
+	console.log("Hello World");
+	getLocData();
+});
+
+function getLocData() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(displayData);
+	}
+}
+
+function displayData(position) {
+	console.log(position);
+}
