d489342ba8d51635c59126067b9e20d1911df912
Evan Kleiner <gkleinereva@gmail.com>
2017-11-13 21:54:59 -0800

    Added basic structure and functionality, started working on CSS animation

--- WeatherApp.js
+++ WeatherApp.js
@@ -1,14 +1,97 @@
+//Global flag to track the current temperature units
+var celcius = true;
+
+//Grabs/displays Location data as soon as the page is loaded
 $(document).ready(function() {
 	console.log("Hello World");
+	
+	//call the changeUnits function whenever one of the units is clicked
+	$(".unit").click(function() {
+		changeUnits();
+	});
+	
 	getLocData();
 });
 
+//Grabs user location data, then calls displayData to show it in the browser
 function getLocData() {
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(displayData);
 	}
 }
 
+//Parses geolocation data and passes lat/long coordinates to function that calls FCC API
 function displayData(position) {
+	console.log("Geolocation return value:");
 	console.log(position);
+	getWeatherData(position.coords.longitude, position.coords.latitude);
+}
+
+//Calls FCC API with lat/long coordinates, then calls updateHTML to display weather
+function getWeatherData(long,lat) {
+	$.ajax({
+		url: "https://fcc-weather-api.glitch.me/api/current",
+		dataType: "json",
+		type: "GET",
+		data: {
+			lon: long,
+			lat: lat
+		},
+		success: function(weatherData) {
+			updateWeatherData(weatherData);
+		}
+	});
+}
+
+//parses FCC return values and updates HTML elements
+function updateWeatherData(weatherData) {
+	console.log("FCC Weather API results: ");
+	console.log(weatherData);
+	$("#location").html(weatherData.name);
+	$("#condition").html(weatherData.weather[0].main);
+	$("#temp").html(weatherData.main.temp + " ");
+	$("#low").html(weatherData.main.temp_min + " ");
+	$("#high").html(weatherData.main.temp_max + " ");
+	$("img").attr("src", weatherData.weather[0].icon);
+
+	//now we're going to update our CSS to do the animation
+	$("div").addClass("textClass");
+	addImageCSS();
+}
+
+function changeUnits () {
+	if (celcius == true) {
+		$("#temp").html(cToF($("#temp").html()));
+		$("#low").html(cToF($("#low").html()));
+		$("#high").html(cToF($("#high").html()));
+		$(".unit").html("&#8457");
+	}
+	else {
+		$("#temp").html(fToC($("#temp").html()));
+		$("#low").html(fToC($("#low").html()));
+		$("#high").html(fToC($("#high").html()));
+		$(".unit").html("&#8451");
+	}
+}
+
+function cToF(cel) {
+	celcius = false;
+	return (cel*9.0/5.0 + 32.0).toFixed(2);
+}
+
+function fToC(fahr) {
+	celcius = true;
+	return ((fahr - 32.0)*5.0/9.0).toFixed(2);
+}
+
+//adds necessary CSS to the image to animate it
+function addImageCSS() {
+	console.log("Hello Animated CSS");
+	$("img").addClass("imageClass");
+	$("img").css("left", $(window).width()/2);
+	$("img").css("top", $(window).height()/2);
+
+	var css=document.styleSheets[0];
+	console.log(css);
+	css.insertRule("body {background-color: red; }");
 }
